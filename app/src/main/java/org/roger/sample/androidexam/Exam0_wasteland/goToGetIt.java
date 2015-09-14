2032fc67d8528e8b0c579871f56330c39d913abe
liren.lrl <liren.lrl@alibaba-inc.com>
2015-09-14 16:53:46 +0800

    add classloader learn

--- app/src/main/java/org/roger/sample/androidexam/Exam0_wasteland/goToGetIt.java
+++ app/src/main/java/org/roger/sample/androidexam/Exam0_wasteland/goToGetIt.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
+import dalvik.system.DexClassLoader;
+
 /**
  * Created by liren on 15/8/26.
  */
@@ -12,7 +14,30 @@ public class goToGetIt extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        testUri();
+//        testUri();
+        classLoaderLearn();
+    }
+
+    private void classLoaderLearn() {
+//        ClassLoader cl = this.getClassLoader();
+        ClassLoader cl = this.getApplication().getClassLoader();
+
+
+        ClassLoader mcl = new DexClassLoader(this.getApplication().getFilesDir().getAbsolutePath() + "/" + "a.apk",this.getApplication().getFilesDir().getAbsolutePath(),null,cl);
+        while(mcl != null) {
+            Log.i("goToGetIt","classloader is " + mcl);
+            mcl = mcl.getParent();
+        }
+        Log.i("goToGetIt","last classloader is " + mcl);
+
+
+
+        while(cl != null) {
+            Log.i("goToGetIt","classloader is " + cl);
+            cl = cl.getParent();
+        }
+        Log.i("goToGetIt","last classloader is " + cl);
+
     }
 
     private void testUri() {
