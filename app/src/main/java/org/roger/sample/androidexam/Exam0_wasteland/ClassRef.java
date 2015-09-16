f34c452243dea79f472508e68d3eb0a89ecbdae3
liren.lrl <liren.lrl@alibaba-inc.com>
2015-09-16 21:11:19 +0800

    add a new class to test classload

--- app/src/main/java/org/roger/sample/androidexam/Exam0_wasteland/ClassRef.java
+++ app/src/main/java/org/roger/sample/androidexam/Exam0_wasteland/ClassRef.java
@@ -1,7 +1,12 @@
 package org.roger.sample.androidexam.Exam0_wasteland;
 
+import android.util.Log;
+
 /**
  * Created by liren on 15/9/16.
  */
 public class ClassRef {
+    static {
+        Log.i("classload", "ClassRef initialization.");
+    }
 }
