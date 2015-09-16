cb1e9e0798c558d9bb2c123149731a11ce55d10b
liren.lrl <liren.lrl@alibaba-inc.com>
2015-09-16 21:10:59 +0800

    update

--- app/src/main/java/org/roger/sample/androidexam/Exam0_wasteland/ChildOfForClassLoadTest.java
+++ app/src/main/java/org/roger/sample/androidexam/Exam0_wasteland/ChildOfForClassLoadTest.java
@@ -1,7 +1,22 @@
 package org.roger.sample.androidexam.Exam0_wasteland;
 
+import android.util.Log;
+
 /**
  * Created by liren on 15/9/16.
  */
-public class ChildOfForClassLoadTest {
+public class ChildOfForClassLoadTest extends ForClassLoadTest {
+
+    public static ClassRef cref;
+
+    static {
+        Log.i("classload", "ChildOfForClassLoadTest's initialization.");
+    }
+
+    @Override
+    public void fun() {
+        Log.i("classload","Child's fun()");
+        cref = new ClassRef();
+        // 这里会在Child's fun()之后，执行ClassRef中的static块。不写new的话，不会初始化。那么如何再现NoClassDefException的问题呢？
+    }
 }
