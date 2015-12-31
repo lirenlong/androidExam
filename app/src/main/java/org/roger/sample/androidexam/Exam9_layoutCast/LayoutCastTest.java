33de589449b45c41649f248201e1c56f104b94a0
liren.lrl <liren.lrl@alibaba-inc.com>
2015-12-31 14:53:54 +0800

    1. 修改一个配置 2. 修改layoutcast的验证代码

---
+++ app/src/main/java/org/roger/sample/androidexam/Exam9_layoutCast/LayoutCastTest.java
@@ -0,0 +1,24 @@
+package org.roger.sample.androidexam.Exam9_layoutCast;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import org.roger.sample.androidexam.R;
+
+/**
+ * Created by liren on 15/11/24.
+ */
+public class LayoutCastTest extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.e9_layoutcast);
+    }
+
+    public void onImgClicked(View view) {
+
+    }
+}
